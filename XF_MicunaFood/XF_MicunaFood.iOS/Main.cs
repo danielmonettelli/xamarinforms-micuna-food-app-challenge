5d0a276ca50af6ba01eabd495eb5601bbf2158e1
Daniel Angel Monettelli L <danielmonetelli@hotmail.com>
2020-12-19 22:14:39 -0500

    Add project files.

---
+++ XF_MicunaFood/XF_MicunaFood.iOS/Main.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UIKit;
+
+namespace XF_MicunaFood.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
