477b6dad51af97463ede6f7646c46312b9611c85
Daniel Angel Monettelli L <danielmonetelli@hotmail.com>
2021-04-29 01:26:46 -0500

    Commit: Xamarin.Essentials "Screenshot" and "Share" on FoodDetailPage.
    1.- Added "icon_screenshot_solid" to the project logic.
    2.- Added *CacheType = "All"* property to images of type "FFImageLoading".
    3.- Added VSM "vsm_fontIconChanged", to switch from *Screenshot* to *Share File*.
    4.- In the code behind "FoodDetailPage", in the "ScreenshotAndShareFile_Tapped" event, the logic of "Screenshot (two modes)" and "File Sharing" was added.
    5.- Added the "Toast Multiplatform" logic.
    6.- Added ImageFromXamarinUI v1.0.0.

--- XF_MicunaFood/XF_MicunaFood/CodeFontIcons/MonettelliFontIcons.cs
+++ XF_MicunaFood/XF_MicunaFood/CodeFontIcons/MonettelliFontIcons.cs
@@ -6,20 +6,21 @@ namespace XF_MicunaFood.CodeFontIcons
 {
     static class MonettelliFontIcons
     {
-        public const string icon_fast_food_line = "\u0061";
-        public const string icon_dessert_line = "\u0062";
-        public const string icon_beer_line = "\u0063";
-        public const string icon_gourmet_food_line = "\u0064";
         public const string icon_heart_line = "\ue800";
         public const string icon_left_arrow_line = "\ue801";
         public const string icon_reloj_line = "\ue802";
-        public const string icon_share_solid = "\ue803";
-        public const string icon_tab_conversation_line = "\ue804";
-        public const string icon_tab_home_line = "\ue805";
-        public const string icon_tab_user_line = "\ue806";
-        public const string icon_tab_wallet_line = "\ue807";
-        public const string icon_add_line = "\ue808";
-        public const string icon_filter_line = "\ue80c";
-        public const string icon_shopping_cart_line = "\ue80e";
+        public const string icon_screenshot_solid = "\ue803";
+        public const string icon_share_solid = "\ue804";
+        public const string icon_shopping_cart_line = "\ue805";
+        public const string icon_tab_conversation_line = "\ue806";
+        public const string icon_tab_home_line = "\ue807";
+        public const string icon_tab_user_line = "\ue808";
+        public const string icon_tab_wallet_line = "\ue809";
+        public const string icon_add_line = "\ue80a";
+        public const string icon_beer_line = "\ue80b";
+        public const string icon_dessert_line = "\ue80c";
+        public const string icon_fast_food_line = "\ue80d";
+        public const string icon_filter_line = "\ue80e";
+        public const string icon_gourmet_food_line = "\ue80f";
     }
 }
